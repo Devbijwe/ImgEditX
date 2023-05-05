ecf272831dbbde4a921613335c6ed9ccccd765d2
Shiva Bijwe <22devendrabijwe@gmail.com>
2023-05-05 18:01:12 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -19,5 +19,5 @@ from views import *
 with app.app_context():
     db.create_all()
 
-if __name__=='__main__':
-    app.run(debug=True,host="0.0.0.0",port=4000)
+# if __name__=='__main__':
+#     app.run(debug=True,host="0.0.0.0",port=4000)
